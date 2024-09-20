fea145d3615ee571851527c9b13fa8e2add10e35
LiamKearn <contact@liamkearn.me>
2024-09-20 20:54:10 +1000

    Install luckperms

--- scripts/start.sh
+++ scripts/start.sh
@@ -102,6 +102,11 @@ ln -s "${STATE_DIR}/banned-ips.json" "${SERVER_DIR}/banned-ips.json"
 ln -s "${STATE_DIR}/banned-players.json" "${SERVER_DIR}/banned-players.json"
 ln -s "${STATE_DIR}/ops.json" "${SERVER_DIR}/ops.json"
 
+# This one is "unique" because of expert developers
+mkdir -p "${SERVER_DIR}/mods"
+ln -s "${STATE_DIR}/luckperms" "${SERVER_DIR}/mods/luckperms"
+chown -h "${APPLICATION_USER}:${APPLICATION_USER}" "${SERVER_DIR}/mods/luckperms"
+
 cat <<EOF > "${SERVER_DIR}/allowed_symlinks.txt"
 ${STATE_DIR}/world
 ${STATE_DIR}/logs
@@ -207,10 +212,13 @@ curl -o "${JAR_PATH}" -OJ "${DOWNLOAD_LOCATION}"
 check_the_sum "${JAR_PATH}" "${SERVER_JAR_CHECKSUM}"
 
 # Now for modifications!
-mkdir -p "${SERVER_DIR}/mods"
-download_mod "https://cdn.modrinth.com/data/P7dR8mSH/versions/qKPgBeHl/fabric-api-0.104.0%2B1.21.1.jar" "b1aeaf90a9af7b5fd4069147bfb8b5bd4c66e4756248ae12fed776e2da694a1a"
+
+# Not yet setup
+# download_mod "https://cdn.modrinth.com/data/PFb7ZqK6/versions/DJbC2aUl/squaremap-fabric-mc1.21.1-1.2.7.jar" "d8b06c000a7d1701deef44effab230d0810300f8b6daeeb01033d2f1d98fc06b"
 # Disabled because of crash, cba to debug atm.
 # download_mod "https://cdn.modrinth.com/data/gvQqBUqZ/versions/5szYtenV/lithium-fabric-mc1.21.1-0.13.0.jar" "10d371fee397bf0306e1e2d863c54c56442bcc2dc6e01603f1469f2fe4910d61"
+
+download_mod "https://cdn.modrinth.com/data/P7dR8mSH/versions/qKPgBeHl/fabric-api-0.104.0%2B1.21.1.jar" "b1aeaf90a9af7b5fd4069147bfb8b5bd4c66e4756248ae12fed776e2da694a1a"
 download_mod "https://cdn.modrinth.com/data/KOHu7RCS/versions/Kxy5mXbm/Moonrise-Fabric-0.1.0-beta.2%2B44f8058.jar" "dfee191fbb525d0af10893aff55da02ee96e91d9e337b9eca75dc9724679a4b5"
 download_mod "https://cdn.modrinth.com/data/fALzjamp/versions/dPliWter/Chunky-1.4.16.jar" "c9f03e322e631ee94ccb8dbf3776859cd12766e513b7533e9f966e799db47937"
 download_mod "https://cdn.modrinth.com/data/s86X568j/versions/uT1cdd3k/ChunkyBorder-1.2.18.jar" "0a4066b36603e1d91fe7d11cce8e2eb066c668828889c866ce08d1baf469f351"
@@ -219,7 +227,7 @@ download_mod "https://cdn.modrinth.com/data/s86X568j/versions/uT1cdd3k/ChunkyBor
 # Geyser returns a UTF8 filename content-disposition header, which is not supported by curl, we need to manually specifiy the filename here.
 download_mod "https://download.geysermc.org/v2/projects/geyser/versions/2.4.3/builds/676/downloads/fabric" "cfb15ad7c1b938af8ad96554d2764549f66899262cc3579fdcdbc94bcc5400a5" "Geyser-Fabric.jar"
 download_mod "https://cdn.modrinth.com/data/bWrNNfkb/versions/wPa1pHZJ/Floodgate-Fabric-2.2.4-b36.jar" "89fcd6add678289a10a45b2976198e43e149b7054c686b5fcb85d039c7b05746"
-# download_mod "https://cdn.modrinth.com/data/PFb7ZqK6/versions/DJbC2aUl/squaremap-fabric-mc1.21.1-1.2.7.jar" "d8b06c000a7d1701deef44effab230d0810300f8b6daeeb01033d2f1d98fc06b"
+download_mod "https://cdn.modrinth.com/data/Vebnzrzj/versions/l47d4ZWk/LuckPerms-Fabric-5.4.140.jar" "3e17d490f87761c174478f68860367610a473ff5c2a9a9daad608773bf0e81bc"
 
 # Start the minecraft server as the application user running in a named screen session
 su - "${APPLICATION_USER}" -c "screen -S mc -d -m ${SERVER_DIR}/start-server.sh"
