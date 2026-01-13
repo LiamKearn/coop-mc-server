88fdc5e09489904b59f84c9b8efa97c7211ac7f2
LiamKearn <contact@liamkearn.me>
2026-01-13 18:02:06 +1100

    Format terraform

--- dev/variables.tf
+++ dev/variables.tf
@@ -5,28 +5,28 @@ variable "aws_region" {
 }
 
 variable "aws_availability_zone" {
-    description = "AWS availability zone"
-    type        = string
-    default     = "us-east-1b"
+  description = "AWS availability zone"
+  type        = string
+  default     = "us-east-1b"
 }
 
 variable "personal_public_key" {
-    description = "Personal public key"
-    type        = string
+  description = "Personal public key"
+  type        = string
 }
 
 variable "personal_ip_cidr" {
-    description = "Personal IP CIDR"
-    type        = string
+  description = "Personal IP CIDR"
+  type        = string
 }
 
 variable "minecraft_ip_address" {
-    description = "Minecraft IP address"
-    type        = string
+  description = "Minecraft IP address"
+  type        = string
 }
 
 variable "minecraft_state_volume_id" {
-    description = "Minecraft state volume ID"
-    type        = string
+  description = "Minecraft state volume ID"
+  type        = string
 }
 
