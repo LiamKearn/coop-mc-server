0382ca665626cce7679df42326f1b6fae3773cc5
LiamKearn <contact@liamkearn.me>
2026-01-14 14:50:53 +1100

    Rename stuff for better finding

--- ami/aws-gameserver.pkr.hcl
+++ ami/aws-gameserver.pkr.hcl
@@ -25,7 +25,7 @@ build {
   ]
 
   provisioner "shell" {
-    script = "scripts/provision.sh"
+    script = "scripts/provision-gameserver.sh"
   }
 }
 
--- modules/proxied_minecraft/gameserver.tf
+++ modules/proxied_minecraft/gameserver.tf
@@ -89,7 +89,7 @@ resource "aws_instance" "gameserver" {
 
   # Nushell command to deregister previous AMI if needed (THIS DOESN'T DELETE SNAPSHOTS):
   # ^aws --region us-east-1 ec2 deregister-image --image-id (^aws --region us-east-1 ec2 describe-images --filters Name=name,Values=minecraft | from json | get Images | first | get ImageId)
-  # Minecraft AMI built with Packer, See: ./ami/aws-minecraft.pkr.hcl
+  # Minecraft AMI built with Packer, See: ./ami/aws-gameserver.pkr.hcl
   ami = "ami-0dad37aad8bc5d67d"
 
   instance_type        = "t4g.large"
