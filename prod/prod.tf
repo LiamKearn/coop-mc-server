1cae9c0790981875f0f219c388df9cae482d7379
LiamKearn <contact@liamkearn.me>
2026-01-15 18:09:50 +1100

    Update terraform and require specific version for new resource

--- prod/prod.tf
+++ prod/prod.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.28.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
+
 }
 
 module "minecraft" {
